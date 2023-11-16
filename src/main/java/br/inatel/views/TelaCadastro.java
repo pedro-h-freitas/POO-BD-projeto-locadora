19c149df596a790f1d7d84e89250a9bf66686157
Pedro Freitas <phfreitas@ges.inatel.br>
2023-11-16 08:27:45 -0300

    adiciona `MenuInicial`, `TelaCadastro` e `TelaLogin`

---
+++ src/main/java/br/inatel/views/TelaCadastro.java
@@ -0,0 +1,14 @@
+package br.inatel.views;
+
+/**
+ * Tela de cadastro, cadastra apenas cliente, funcionario ou gerente
+ */
+public class TelaCadastro {
+    /**
+     *
+     */
+    public static void run(){
+        // TODO
+        System.out.println("TELA DE CADASTRO");
+    }
+}
---
+++ src/main/java/br/inatel/views/TelaLogin.java
@@ -0,0 +1,14 @@
+package br.inatel.views;
+
+/**
+ * Tela de login, loga um cliente, funcionario ou gerente
+ */
+public class TelaLogin {
+    /**
+     * Exibe a Tela de Login
+     */
+    public static void run() {
+        // TODO
+        System.out.println("TELA DE LOGIN");
+    }
+}
