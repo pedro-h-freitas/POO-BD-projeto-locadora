bbea8f5fc6ad3595bde271c06fb4548503229280
Pedro Freitas <phfreitas@ges.inatel.br>
2023-11-16 14:24:12 -0300

    criado classe `Context` para controlar o contexto da sessao (usuario logado, locadora)

--- src/main/java/br/inatel/Main.java
+++ src/main/java/br/inatel/Main.java
@@ -1,10 +1,12 @@
 package br.inatel;
 
+import br.inatel.controllers.Context;
 import br.inatel.views.MenuInicial;
 
 import java.util.Scanner;
 
 public class Main {
+    public static Context context = new Context(); // contexto de usuário logado
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
---
+++ src/main/java/br/inatel/controllers/Context.java
@@ -0,0 +1,58 @@
+package br.inatel.controllers;
+
+/**
+ * Classe para criar o contexto da sessão
+ */
+public class Context {
+    private int userId;
+    private String userType; // (Cliente, Funcionario, Gerente, Dono)
+    private int locadoraId;
+
+    /**
+     * Getter do id do usuario da sessao
+     * @return id do usuario da sessao
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * Setter do id do usuario da sessao
+     * @param userId id do usuario da sessao
+     */
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * Getter do tipo de usuario da sessao
+     * @return tipo de usuario da sessao (Cliente, Funcionario, Gerente, Dono)
+     */
+    public String getUserType() {
+        return userType;
+    }
+
+    /**
+     * Setter do tipo de usuario da sessao
+     * @param userType tipo de usuario da sessao (Cliente, Funcionario, Gerente, Dono)
+     */
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    /**
+     * Getter do id da locadora da sessao
+     * @return id da locadora da sessao
+     */
+    public int getLocadoraId() {
+        return locadoraId;
+    }
+
+    /**
+     * Setter do id da locadora da sessao
+     * @param locadoraId id da locadora da sessao
+     */
+    public void setLocadoraId(int locadoraId) {
+        this.locadoraId = locadoraId;
+    }
+}
