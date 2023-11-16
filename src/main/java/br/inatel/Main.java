9d9de317724291fe7410934d3dc21576828d9a2c
Pedro Freitas <phfreitas@ges.inatel.br>
2023-11-16 11:52:39 -0300

    inicia `Scanner` e implementa `MenuInicial` em `Main`

--- src/main/java/br/inatel/Main.java
+++ src/main/java/br/inatel/Main.java
@@ -1,7 +1,21 @@
 package br.inatel;
 
+import br.inatel.views.MenuInicial;
+
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean run = true;
+
+        // loop de execução
+        System.out.println("----- Bem vindo à POO-BD locadora -----");
+        while (run) {
+            run = MenuInicial.run();
+        }
+        System.out.println("--------- Programa Finalizado ---------");
+        sc.close();
     }
 }
