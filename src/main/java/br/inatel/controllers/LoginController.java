752b46ed4067c90c14677e1eb2599eba0287464d
Pedro Freitas <phfreitas@ges.inatel.br>
2023-11-16 21:39:04 -0300

    `TelaLogin` implementado

---
+++ src/main/java/br/inatel/controllers/LoginController.java
@@ -0,0 +1,43 @@
+package br.inatel.controllers;
+
+import br.inatel.Main;
+import br.inatel.controllers.DAO.ClienteDAO;
+import br.inatel.controllers.DAO.DAOWithId;
+import br.inatel.controllers.DAO.FuncionarioDAO;
+import br.inatel.models.Cliente;
+import br.inatel.models.Funcionario;
+
+public class LoginController {
+    public int login(String id, String senha) {
+        if (id.charAt(0) == '1') {
+            FuncionarioDAO dao = new FuncionarioDAO();
+            Funcionario funcionario = dao.selectById(Integer.parseInt(id));
+
+            if (funcionario == null)
+                return -1;
+            if (!funcionario.getSenha().equals(senha))
+                return -2;
+
+            Main.context.setUserId(funcionario.getId());
+            Main.context.setUserType("Funcionario");
+            Main.context.setLocadoraId(funcionario.getIdLocadora());
+
+            return 1;
+        }
+        else if (id.charAt(0) == '2') {
+            ClienteDAO dao = new ClienteDAO();
+            Cliente cliente = dao.selectById(Integer.parseInt(id));
+
+            if (cliente == null)
+                return -1;
+            if (!cliente.getSenha().equals(senha))
+                return -2;
+
+            Main.context.setUserId(cliente.getId());
+            Main.context.setUserType("Cliente");
+
+            return 2;
+        }
+        return -3;
+    }
+}
--- src/main/java/br/inatel/views/TelaLogin.java
+++ src/main/java/br/inatel/views/TelaLogin.java
@@ -1,5 +1,8 @@
 package br.inatel.views;
 
+import br.inatel.Main;
+import br.inatel.controllers.LoginController;
+
 /**
  * Tela de login, loga um cliente, funcionario ou gerente
  */
@@ -9,6 +12,33 @@ public class TelaLogin {
      */
     public static void render() {
         // TODO
-        System.out.println("TELA DE LOGIN");
+        String id, senha;
+        LoginController l = new LoginController();
+
+        System.out.println("--------------- LOGIN -----------------");
+
+        System.out.print("Id: ");
+        id = Main.sc.nextLine();
+
+        System.out.print("Senha: ");
+        senha = Main.sc.nextLine();
+
+        switch (l.login(id, senha)) {
+            case 1:
+                System.out.println("Funcionário");
+                System.out.println("ID: " + Main.context.getUserId());
+                break;
+            case 2:
+                System.out.println("Cliente");
+                break;
+            case -1:
+                System.out.println("ID INVÁLIDO");
+                break;
+            case -2:
+                System.out.println("SENHA INVÁLIDA");
+                break;
+        }
+
+        System.out.println("---------------------------------------");
     }
 }
