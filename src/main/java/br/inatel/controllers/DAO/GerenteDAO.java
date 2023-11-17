eae9820680b85856d4f906617b5af045280b0b11
Pedro Freitas <phfreitas@ges.inatel.br>
2023-11-16 22:11:52 -0300

    adiciona contexto de `Gerente`

--- src/main/java/br/inatel/controllers/DAO/GerenteDAO.java
+++ src/main/java/br/inatel/controllers/DAO/GerenteDAO.java
@@ -1,5 +1,6 @@
 package br.inatel.controllers.DAO;
 
+import br.inatel.models.Funcionario;
 import br.inatel.models.Gerente;
 
 import java.sql.PreparedStatement;
@@ -8,7 +9,7 @@ import java.sql.SQLException;
 /**
  * Class for CREATE, READ, UPDATE objects of the table "gerente"
  */
-public class GerenteDAO extends ConnectionDAO<Gerente> {
+public class GerenteDAO extends DAOWithId<Gerente> {
 
     /**
      * Método para obter a query de inserção específica do Gerente
@@ -28,5 +29,30 @@ public class GerenteDAO extends ConnectionDAO<Gerente> {
     @Override
     protected void setInsertValues(PreparedStatement pst, Gerente gerente) throws SQLException {
         pst.setInt(1, gerente.getIdFuncionario());
+    }
+
+    /**
+     * Método para obter a query de seleção por id do Gerente
+     * @return "SELECT * FROM funcionario WHERE id_funcionario=?"
+     */
+    @Override
+    protected String getSelectByIdQuery() {
+        return "SELECT * FROM funcionario WHERE id_funcionario=?";
+    }
+
+    /**
+     * Função para mapear um ResultSet em um model.Gerente
+     * @return Objeto Gerente mapeado do ResultSet
+     */
+    @Override
+    protected Gerente getMapper() {
+        try {
+            return new Gerente(
+                    rs.getInt("id_funcionario")
+            );
+        } catch (SQLException e) {
+            System.out.println("Erro: " + e.getMessage());
+            return null;
+        }
     }
 }
--- src/main/java/br/inatel/controllers/LoginController.java
+++ src/main/java/br/inatel/controllers/LoginController.java
@@ -4,8 +4,10 @@ import br.inatel.Main;
 import br.inatel.controllers.DAO.ClienteDAO;
 import br.inatel.controllers.DAO.DAOWithId;
 import br.inatel.controllers.DAO.FuncionarioDAO;
+import br.inatel.controllers.DAO.GerenteDAO;
 import br.inatel.models.Cliente;
 import br.inatel.models.Funcionario;
+import br.inatel.models.Gerente;
 
 /**
  * Classe para manipular as ações na tela de Login
@@ -15,27 +17,36 @@ public class LoginController {
      * Realiza um login
      * @param id id do usuario
      * @param senha senha do usuario
-     * @return contexto (1: Funcionario | 2: Cliente | 3: Gerente | -1: id não encontrado | -2: senha incorreta)
+     * @return contexto (1: Gerente | 2: Funcionario | 3: Cliente | -1: id não encontrado | -2: senha incorreta)
      */
     public int login(String id, String senha) {
         if (id.charAt(0) == '1') {
-            FuncionarioDAO dao = new FuncionarioDAO();
-            Funcionario funcionario = dao.selectById(Integer.parseInt(id));
+            FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+            Funcionario funcionario = funcionarioDAO.selectById(Integer.parseInt(id));
 
             if (funcionario == null)
                 return -1;
             if (!funcionario.getSenha().equals(senha))
                 return -2;
+            GerenteDAO gerenteDAO = new GerenteDAO();
+            Gerente gerente = gerenteDAO.selectById(Integer.parseInt(id));
+            if (!(gerente == null)) {
+                Main.context.setUserId(funcionario.getId());
+                Main.context.setUserType("Funcionario");
+                Main.context.setLocadoraId(funcionario.getIdLocadora());
+
+                return 1;
+            }
 
             Main.context.setUserId(funcionario.getId());
             Main.context.setUserType("Funcionario");
             Main.context.setLocadoraId(funcionario.getIdLocadora());
 
-            return 1;
+            return 2;
         }
         else if (id.charAt(0) == '2') {
-            ClienteDAO dao = new ClienteDAO();
-            Cliente cliente = dao.selectById(Integer.parseInt(id));
+            ClienteDAO clienteDAO = new ClienteDAO();
+            Cliente cliente = clienteDAO.selectById(Integer.parseInt(id));
 
             if (cliente == null)
                 return -1;
@@ -45,7 +56,7 @@ public class LoginController {
             Main.context.setUserId(cliente.getId());
             Main.context.setUserType("Cliente");
 
-            return 2;
+            return 3;
         }
         return -1;
     }
