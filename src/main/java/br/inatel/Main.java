6129d04a69e1a4b7ddbe529efc789356fdca2a8d
Pedro Freitas <phfreitas@ges.inatel.br>
2023-11-20 14:10:30 -0300

    teste `TelaCliente`

--- src/main/java/br/inatel/Main.java
+++ src/main/java/br/inatel/Main.java
@@ -1,7 +1,7 @@
 package br.inatel;
 
 import br.inatel.controllers.Context;
-import br.inatel.views.TelaCliente;
+import br.inatel.views.*;
 
 import java.util.Scanner;
 
@@ -13,24 +13,27 @@ public class Main {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        /*TelaInicial telaInicial = new TelaInicial();
+        MenuInicial menuInicial = new MenuInicial();
         boolean run = true;
-
+/*
         // loop de execução
         System.out.println("----- Bem vindo à POO-BD locadora -----");
         while (run) {
-            run = telaInicial.render();
+            run = menuInicial.render();
         }
         System.out.println("--------- Programa Finalizado ---------");
-        sc.close();
+        */
 
-         */
-        TelaCliente telaCliente = new TelaCliente();
+        Menu menu = new MenuCliente();
 
-        context.setUserId(201);
+        context.setUserId(205);
         context.setUserType(Context.CLIENTE);
         context.setLocadoraId(-1);
 
-        telaCliente.render();
+        while (run) {
+            run = menu.render();
+        }
+
+        sc.close();
     }
 }
