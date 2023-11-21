cf24076e9d1839b5b28bbec47492a1bacb858c71
Pedro Freitas <phfreitas@ges.inatel.br>
2023-11-21 11:48:22 -0300

    cria e implementa `FilmeAlugar`

---
+++ src/main/java/br/inatel/models/FilmeAlugar.java
@@ -0,0 +1,62 @@
+package br.inatel.models;
+
+import java.time.Year;
+
+public class FilmeAlugar {
+    private int id;
+    private String nome;
+    private int ano;
+    private String generos;
+    private int preco;
+
+    public FilmeAlugar(int id, String nome, int ano, String generos) {
+        this.id = id;
+        this.nome = nome;
+        this.ano = ano;
+        this.generos = generos;
+
+        preco = 10 - (Year.now().getValue() - this.getAno());
+        if (preco < 5) preco = 5;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getGeneros() {
+        return generos;
+    }
+
+    public void setGeneros(String generos) {
+        this.generos = generos;
+    }
+
+    public int getPreco() {
+        return preco;
+    }
+
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
+}
