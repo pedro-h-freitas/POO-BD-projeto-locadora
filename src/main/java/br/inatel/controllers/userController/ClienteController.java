afb834bd978ca73c2576ba1c5752e3708dbadbea
Pedro Freitas <phfreitas@ges.inatel.br>
2023-11-21 11:47:56 -0300

    implementa `alugar`, `hasAluguel`, `getFilmes`

--- src/main/java/br/inatel/controllers/userController/ClienteController.java
+++ src/main/java/br/inatel/controllers/userController/ClienteController.java
@@ -1,7 +1,13 @@
 package br.inatel.controllers.userController;
 
 import br.inatel.Main;
+import br.inatel.controllers.DAO.AluguelDAO;
+import br.inatel.controllers.DAO.FilmeAlugadoDAO;
+import br.inatel.controllers.DAO.FilmeDAO;
 import br.inatel.controllers.DAO.LocadoraDAO;
+import br.inatel.models.Aluguel;
+import br.inatel.models.FilmeAlugado;
+import br.inatel.models.FilmeAlugar;
 import br.inatel.models.Locadora;
 
 import java.util.ArrayList;
@@ -15,6 +21,36 @@ public class ClienteController {
     public String getNomeLocadora() {
         LocadoraDAO locadoraDAO = new LocadoraDAO();
         return locadoraDAO.selectNome(Main.context.getLocadoraId());
+    }
+
+    public ArrayList<FilmeAlugar> getFilmes() {
+        FilmeDAO filmeDAO = new FilmeDAO();
+        return filmeDAO.selectFilmeAlugar(Main.context.getLocadoraId());
+    }
+
+    public boolean hasAluguel() {
+        AluguelDAO aluguelDAO = new AluguelDAO();
+
+        String locadoraId = Integer.toString(Main.context.getLocadoraId());
+        String clienteId = Integer.toString(Main.context.getUserId());
+
+        int id = Integer.parseInt(locadoraId + clienteId);
+
+        return aluguelDAO.hasAluguel(id);
+    }
+
+    public void alugar(ArrayList<FilmeAlugar> filmes) {
+        AluguelDAO aluguelDAO = new AluguelDAO();
+        FilmeAlugadoDAO filmeAlugadoDAO = new FilmeAlugadoDAO();
+
+        Aluguel aluguel = new Aluguel(Main.context.getUserId(), Main.context.getLocadoraId());
+        int aluguelId = aluguelDAO.insert(aluguel);
+
+        for (FilmeAlugar filme :
+                filmes) {
+            FilmeAlugado filmeAlugado = new FilmeAlugado(aluguelId, filme.getId());
+            filmeAlugadoDAO.insert(filmeAlugado);
+        }
     }
 
 }
