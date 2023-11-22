223f9995e51b431db6da5deb1fafc4512e899e6f
Pedro Freitas <phfreitas@ges.inatel.br>
2023-11-22 15:29:12 -0300

    implementa `FormEditarCliente.render()`

--- src/main/java/br/inatel/views/forms/FormEditarCliente.java
+++ src/main/java/br/inatel/views/forms/FormEditarCliente.java
@@ -11,6 +11,63 @@ public class FormEditarCliente extends Form {
 
     @Override
     public boolean render() {
-        return false;
+        FormsController controller = new FormsController();
+        Cliente cliente = controller.getCliente(idCliente);
+
+        System.out.println("--------------- EDITAR ----------------");
+        String nome = inputEditCampo("Nome", cliente.getNome());
+        String cpf = inputEditCampo("Cpf", cliente.getCpf());
+        String senha = inputEditCampo("Senha", cliente.getSenha());
+        String endereco = inputEditCampo("Endereco", cliente.getEndereco());
+        String telefone = inputEditCampo("Telefone", cliente.getTelefone());
+        String email = inputEditCampo("Email", cliente.getEmail());
+        System.out.println("---------------------------------------");
+
+        nome = checkCampo(nome, cliente.getNome());
+        cpf = checkCampo(cpf, cliente.getCpf());
+        senha = checkCampo(senha, cliente.getSenha());
+        endereco = checkCampo(endereco, cliente.getEndereco());
+        telefone = checkCampo(telefone, cliente.getTelefone());
+        email = checkCampo(email, cliente.getEmail());
+
+        boolean b = controller.updateCliente(new Cliente(
+                idCliente,
+                nome,
+                cpf,
+                senha,
+                endereco,
+                telefone,
+                email
+        ));
+
+        if (b)
+            printVerde("Edições concluidas\n");
+        else
+            printVermelho("Falha na edição\n");
+
+        return b;
+    }
+
+    private String inputEditCampo(String nomeCampo, String valAntigo) {
+        String campo;
+        String label;
+
+        if (valAntigo == null)
+            label = nomeCampo + " (opcional): ";
+        else
+            label = nomeCampo + " (" + valAntigo + "): ";
+
+        campo = stringInput(label);
+        if (campo.isEmpty()) {
+            campo = null;
+        }
+
+        return campo;
+    }
+
+    private String checkCampo(String valNovo, String valAntigo) {
+        if (valNovo == null)
+            return valAntigo;
+        return valNovo;
     }
 }
