d5ad4f93e20f75a674544a7423a4b03f7d2817f7
Pedro Freitas <phfreitas@ges.inatel.br>
2023-11-22 15:29:02 -0300

    implementa `MenuFuncionarioCliente.render()`

--- src/main/java/br/inatel/views/funcionario/MenuFuncionarioCliente.java
+++ src/main/java/br/inatel/views/funcionario/MenuFuncionarioCliente.java
@@ -1,10 +1,20 @@
 package br.inatel.views.funcionario;
 
+import br.inatel.controllers.userController.FuncionarioController;
+import br.inatel.models.Cliente;
 import br.inatel.views.Menu;
+import br.inatel.views.Tela;
+import br.inatel.views.forms.FormCadastroCliente;
+import br.inatel.views.forms.FormDeletarCliente;
+import br.inatel.views.forms.FormEditarCliente;
+
+import java.util.ArrayList;
 
 public class MenuFuncionarioCliente extends Menu {
     @Override
     public boolean render() {
+        FuncionarioController controller = new FuncionarioController();
+        Tela tela;
         int op;
 
         System.out.println("-------------- Clientes ---------------");
@@ -16,6 +26,57 @@ public class MenuFuncionarioCliente extends Menu {
 
         op = intInput();
 
+        tela = null;
+        int idCliente;
+        switch (op) {
+            case 1:
+                tela = new FormCadastroCliente();
+                break;
+            case 2:
+                idCliente = escolherCliente(controller);
+                if (idCliente == 0) break;
+
+                tela = new FormEditarCliente(idCliente);
+                break;
+            case 3:
+                idCliente = escolherCliente(controller);
+                if (idCliente == 0) break;
+
+                tela = new FormDeletarCliente(idCliente);
+                break;
+            default:
+                printOpcaoInvalida();
+                break;
+        }
+
+        if (tela != null) tela.render();
+
         return false;
+    }
+
+    private int escolherCliente(FuncionarioController controller) {
+        int idCliente;
+        ArrayList<Cliente> clientes = controller.getAllClientes();
+        ArrayList<Integer> listaId = new ArrayList<>();
+
+        System.out.println("---------------------------------------");
+        for (Cliente c :
+                clientes) {
+            listaId.add(c.getId());
+
+            printOpcao(c.getId() + "", c.getNome() + " (" + c.getCpf() + ")");
+        }
+        printOpcao("0", "Cancelar");
+        listaId.add(0);
+        System.out.println("---------------------------------------");
+
+        while (true) {
+            idCliente = intInput("Id: ");
+            if (listaId.contains(idCliente)) break;
+
+            printVermelho("Id inválido\n");
+        }
+
+        return idCliente;
     }
 }
