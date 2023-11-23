e0990253932a1377aafac628740131d6c3f34d1c
Pedro Freitas <phfreitas@ges.inatel.br>
2023-11-23 14:41:17 -0300

    adiociona `deleteById()` em `FilmeDAO`

--- src/main/java/br/inatel/controllers/DAO/FilmeDAO.java
+++ src/main/java/br/inatel/controllers/DAO/FilmeDAO.java
@@ -1,6 +1,7 @@
 package br.inatel.controllers.DAO;
 
 import br.inatel.models.Filme;
+import br.inatel.views.utils.ColorPrinter;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -31,4 +32,57 @@ public class FilmeDAO extends ConnectionDAO<Filme> {
         pst.setInt(2, filme.getIdLocadora());
         pst.setInt(3, filme.getIdInfoFilme());
     }
+
+    //DELETE
+    public boolean deleteById(int id) {
+        connectToDB();
+
+        String sql = "DELETE FROM filme where id=?";
+        try {
+            pst = con.prepareStatement(sql);
+            pst.setInt(1, id);
+            pst.execute();
+            sucesso = true;
+        } catch (SQLException e) {
+            ColorPrinter.printErro(e);
+            sucesso = false;
+        } finally {
+            try {
+                con.close();
+                pst.close();
+            } catch (SQLException e) {
+                ColorPrinter.printErro(e);
+            }
+        }
+        return sucesso;
+    }
+
+    public boolean removeQnt(int id, int qnt) {
+        connectToDB();
+        String sql = """
+                UPDATE filme\s
+                SET n_copias=n_copias-?, n_disponiveis=n_disponiveis-?
+                WHERE id=?;
+                """;
+        try {
+            pst = con.prepareStatement(sql);
+            pst.setInt(1, qnt);
+            pst.setInt(2, qnt);
+            pst.setInt(3, id);
+            pst.execute();
+            sucesso = true;
+        } catch (SQLException e) {
+            ColorPrinter.printErro(e);
+            sucesso = false;
+        } finally {
+            try {
+                con.close();
+                pst.close();
+            } catch (SQLException e) {
+                ColorPrinter.printErro(e);
+            }
+        }
+        return sucesso;
+    }
+
 }
