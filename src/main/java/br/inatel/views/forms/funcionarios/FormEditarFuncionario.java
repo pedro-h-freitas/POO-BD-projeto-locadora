e1f4345aadcc51691001dcdb3403308adaab1f67
Pedro Freitas <phfreitas@ges.inatel.br>
2023-11-24 11:58:01 -0300

    implementa `FormEditarFuncionario`

--- src/main/java/br/inatel/views/forms/funcionarios/FormEditarFuncionario.java
+++ src/main/java/br/inatel/views/forms/funcionarios/FormEditarFuncionario.java
@@ -1,5 +1,7 @@
 package br.inatel.views.forms.funcionarios;
 
+import br.inatel.models.Cliente;
+import br.inatel.models.Funcionario;
 import br.inatel.views.forms.Form;
 
 public class FormEditarFuncionario extends Form {
@@ -12,6 +14,55 @@ public class FormEditarFuncionario extends Form {
 
     @Override
     public boolean render() {
-        return false;
+        Funcionario funcionario = controller.getFuncionario(idFuncionario);
+
+        System.out.println("--------------- EDITAR ----------------");
+        String nome = inputEditCampo("Nome", funcionario.getNome());
+        String telefone = inputEditCampo("telefone", funcionario.getTelefone());
+        String senha = inputEditCampo("Senha", funcionario.getSenha());
+        System.out.println("---------------------------------------");
+
+        nome = checkCampo(nome, funcionario.getNome());
+        telefone = checkCampo(telefone, funcionario.getTelefone());
+        senha = checkCampo(senha, funcionario.getSenha());
+
+        boolean b = controller.updateFuncionario(new Funcionario(
+                idFuncionario,
+                nome,
+                telefone,
+                funcionario.getSalario(),
+                senha,
+                funcionario.getIdLocadora()
+        ));
+
+        if (b)
+            printVerde("Edições concluidas\n");
+        else
+            printVermelho("Falha na edição\n");
+
+        return b;
+    }
+
+    private String inputEditCampo(String nomeCampo, String valAntigo) {
+        String campo;
+        String label;
+
+        if (valAntigo == null)
+            label = nomeCampo + " (opcional): ";
+        else
+            label = nomeCampo + " (" + valAntigo + "): ";
+
+        campo = stringInput(label);
+        if (campo.isEmpty()) {
+            campo = null;
+        }
+
+        return campo;
+    }
+
+    private String checkCampo(String valNovo, String valAntigo) {
+        if (valNovo == null)
+            return valAntigo;
+        return valNovo;
     }
 }
