22a50566b05f85d5eb8a73636f301c485c317ae9
Pedro Freitas <phfreitas@ges.inatel.br>
2023-11-24 10:38:24 -0300

    implementa `getAllFuncionarios()`

--- src/main/java/br/inatel/controllers/userController/GerenteController.java
+++ src/main/java/br/inatel/controllers/userController/GerenteController.java
@@ -1,4 +1,14 @@
 package br.inatel.controllers.userController;
 
+import br.inatel.models.DAO.userDAO.FuncionarioUserDAO;
+import br.inatel.models.Funcionario;
+
+import java.util.ArrayList;
+
 public class GerenteController extends FuncionarioController {
+    public ArrayList<Funcionario> getAllFuncionarios() {
+        FuncionarioUserDAO funcionarioUserDAO = new FuncionarioUserDAO();
+        return funcionarioUserDAO.selectAll();
+    }
+
 }
