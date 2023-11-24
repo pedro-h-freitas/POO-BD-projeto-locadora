b56e8d2a9da95b8a522843f604f31acfd4ac6c3a
Pedro Freitas <phfreitas@ges.inatel.br>
2023-11-24 11:49:20 -0300

    implementa `FormCadastroFuncionario`

--- src/main/java/br/inatel/views/forms/funcionarios/FormCadastroFuncionario.java
+++ src/main/java/br/inatel/views/forms/funcionarios/FormCadastroFuncionario.java
@@ -1,10 +1,37 @@
 package br.inatel.views.forms.funcionarios;
 
+import br.inatel.Main;
+import br.inatel.models.Funcionario;
 import br.inatel.views.forms.Form;
 
 public class FormCadastroFuncionario extends Form {
     @Override
     public boolean render() {
-        return false;
+        // TODO fazer validação dos dados
+        String nome;
+        String telefone;
+        int salario;
+        String senha;
+        int idLocadora;
+
+        System.out.println("-------------- CADASTRO ---------------");
+
+        nome = inputCampoObrigatorio("Nome");
+        telefone = inputCampoObrigatorio("Telefone");
+        salario = Integer.parseInt(inputCampoObrigatorio("Salario"));
+        senha = inputCampoObrigatorio("Senha");
+        idLocadora = Main.context.getLocadoraId();
+
+        controller.cadastroFuncionario(new Funcionario(
+                nome,
+                telefone,
+                salario,
+                senha,
+                idLocadora
+        ));
+
+        printVerde("Funcionario cadastrado com sucesso\n");
+
+        return true;
     }
 }
